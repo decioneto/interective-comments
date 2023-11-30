30a66e47e6c867436077fe280b0cd54db75c0551
Décio Neto <decio.neto@muralis.com.br>
2023-11-30 17:41:40 -0300

    setup project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,25 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'moderate-blue': '#5457B6',
+                'light-grayish-blue': '#C3C4EF',
+                'soft-red': '#ED6468',
+                'pale-red': '#FFB8BB',
+                'dark-blue': '#324152',
+                'grayish-blue': '#67727E',
+                'light-gray': '#EAECF1',
+                'very-light': '#F5F6FA',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
 export default config
